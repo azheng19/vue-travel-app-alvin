b17b3ea169bd58f502ebfdb60e6fcacf6ce6e22f
Alvin Zheng <azheng2019@126.com>
2022-06-04 14:54:30 +0800

    use dynamic view in TheNavigation

--- src/router/index.js
+++ src/router/index.js
@@ -11,34 +11,6 @@ const routes = [
     component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
-  {
-    path: '/brazil',
-    name: 'brazil',
-    component: () => import(/* webpackChunkName: "brazil" */ '../views/BrazilView.vue'),
-  },
-  {
-    path: '/hawaii',
-    name: 'hawaii',
-    component: () => import(/* webpackChunkName: "hawaii" */ '../views/HawaiiView.vue'),
-  },
-  {
-    path: '/jamaica',
-    name: 'jamaica',
-    component: () => import(/* webpackChunkName: "jamaica" */ '../views/JamaicaView.vue'),
-  },
-  {
-    path: '/panama',
-    name: 'panama',
-    component: () => import(/* webpackChunkName: "panama" */ '../views/PanamaView.vue'),
-  },
-  {
     path: '/details/:id',
     name: 'DestinationDetails',
     component: () => import(/* webpackChunkName: "DestinationDetails" */ '../views/DestinationDetailsView.vue'),
