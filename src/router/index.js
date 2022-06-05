a52e7ef7bd31fa9cdde7f37d6556ba199dfb91f6
Alvin Zheng <azheng2019@126.com>
2022-06-05 20:58:50 +0800

    create a 404 Not Found route and page

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import store from '@/store';
 import VueRouter from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
@@ -24,6 +25,22 @@ const routes = [
         component: () => import(/* webpackChunkName: "ExperienceDetails" */ '../views/ExperienceDetailsView.vue'),
       },
     ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        (destination) => destination.slug === to.params.slug,
+      );
+      if (exists) {
+        next();
+      } else {
+        next({ name: 'notFound' });
+      }
+    },
+  },
+  {
+    path: '/404',
+    alias: '*',
+    name: 'notFound',
+    component: () => import(/* webpackChunkName: "NotFound" */ '../views/NotFound.vue'),
   },
 ];
 
